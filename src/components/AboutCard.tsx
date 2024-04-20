2e7e6558034d0ac11e2d1f7d2061b1fd7e74ec8d
EthanWakeford <ethanwakeford@gmail.com>
2024-04-20 16:57:38 -0500

    made about card smaller

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,4 +1,4 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown from 'react-markdown';
 
 interface AboutCardProps {
   aboutTitle: string;
@@ -15,12 +15,12 @@ const AboutCard: React.FC<AboutCardProps> = ({
 }) => {
   return (
     <>
-      <div className='h-[75vh] bg-ltbg1 px-[10vw] py-16 text-center lg:px-[20vw] lg:py-24 dark:bg-dkbg1'>
-        <div className='flex h-[100%] max-h-screen flex-col items-center justify-around overflow-auto'>
+      <div className='h-[70vh] lg:h-[60vh] bg-ltbg1 px-[10vw] py-16 text-center lg:px-[20vw] lg:py-24 dark:bg-dkbg1'>
+        <div className='flex h-[100%] max-h-screen flex-col items-center justify-between overflow-auto'>
           <h2 className='text-2xl text-black lg:text-4xl dark:text-white'>
             {aboutTitle}
           </h2>
-          <ReactMarkdown className='line-clamp-3 text-lg text-black lg:text-2xl dark:text-white'>
+          <ReactMarkdown className='line-clamp-3 text-lg text-black lg:text-2xl dark:text-white block'>
             {aboutBody}
           </ReactMarkdown>
           <a
