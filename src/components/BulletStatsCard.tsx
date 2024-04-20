bbe9f680049d52fb9df40b5c630c743ac8bbed32
EthanWakeford <ethanwakeford@gmail.com>
2024-04-20 16:46:22 -0500

    styling for mobile

--- src/components/BulletStatsCard.tsx
+++ src/components/BulletStatsCard.tsx
@@ -14,19 +14,16 @@ const BulletStatsCard: React.FC<Props> = ({
   console.log(bg_image, image_alt_text);
   return (
     <div
-      className={`h-[50vh] text-black lg:px-32  dark:text-white bg-center bg-cover flex justify-center`}
+      className={`h-[50vh] text-black lg:px-32 dark:text-white bg-center bg-cover flex justify-center`}
       style={{ backgroundImage: `url('${bg_image}')` }}
     >
       <div
-        className={`flex h-full flex-col items-center justify-around px-16 max-w-[800px] brightness-100 bg-dkbg2 bg-opacity-90`}
+        className={`flex h-full flex-col items-center justify-around px-8 lg:px-16 py-8 max-w-[800px] brightness-100 bg-dkbg2 bg-opacity-90`}
       >
-        <h3
-          className='text-3xl text-primary'
-          dangerouslySetInnerHTML={{ __html: title }}
-        ></h3>
-        <div className='grid grid-cols-2 w-full gap-x-8 text-center'>
+        <h3 className='lg:text-3xl text-2xl text-primary pb-4'>{title}</h3>
+        <div className='grid grid-cols-2 w-full gap-x-4 lg:gap-x-8 text-center items-center'>
           {body.map((bullet) => (
-            <p className='text-lg py-2'>{bullet}</p>
+            <p className='lg:text-lg text-base py-2'>{bullet}</p>
           ))}
         </div>
       </div>
--- src/components/TextImageCard.tsx
+++ src/components/TextImageCard.tsx
@@ -21,15 +21,15 @@ const TextImagecard: React.FC<Props> = ({
 }) => {
   return (
     <div className='flex justify-center dark:bg-dkbg1'>
-      <div className='grid h-[50vh] max-w-[1600px] grid-rows-2 lg:h-[40vh] lg:grid-cols-3 lg:grid-rows-1 transition-all'>
+      <div className='lg:grid max-w-[1200px] flex flex-col lg:h-[40vh] lg:grid-cols-3 lg:grid-rows-1 transition-all'>
         <img
           src={imageLink}
           alt={imageAltText}
-          className={`${reversed && 'lg:order-1'} from-dkbg1 box-border h-full w-full object-cover lg:col-span-1`}
+          className={`${reversed && 'lg:order-1'} from-dkbg1 box-border h-[40vh] lg:h-full w-full object-cover lg:col-span-1`}
         />
         <div className='flex flex-col justify-between bg-ltbg2 py-8  text-center text-black lg:col-span-2 lg:px-6 lg:py-8 dark:bg-dkbg1 dark:text-white transition-all'>
-          <h3 className='text-lg lg:text-3xl text-primary'>{title}</h3>
-          <ReactMarkdown className='text-base lg:text-lg truncate text-left px-12'>
+          <h3 className='text-lg lg:text-3xl pb-4 text-primary'>{title}</h3>
+          <ReactMarkdown className='text-base lg:text-lg lg:px-12 px-6 lg:text-left text-center'>
             {bodyText.toUpperCase()}
           </ReactMarkdown>
           {link && (
