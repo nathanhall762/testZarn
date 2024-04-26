29c936586b7b38d5dd3d85e4c53e32d8d8eb8dce
EthanWakeford <ethanwakeford@gmail.com>
2024-04-25 21:43:27 -0500

    added luxury brands card

---
+++ src/components/LuxuryBrandsCard.tsx
@@ -0,0 +1,40 @@
+import ReactMarkdown from 'react-markdown';
+
+interface props {
+  heading: string;
+  brands: string[];
+  subheading: string;
+  bgImage: string;
+}
+
+const LuxuryBrandsCard: React.FC<props> = ({
+  heading,
+  brands,
+  subheading,
+  bgImage,
+}) => {
+  return (
+    <div
+      className='bg-cover bg-center z-0'
+      style={{ backgroundImage: `url('${bgImage}')` }}
+    >
+      <div className='absolute size-full bg-dkbg1 opacity-70 z-10'></div>
+      <div className='relative flex gap-4 flex-col text-center px-8 py-16 z-20 opacity-100 justify-center'>
+        <h2 className='lg:text-3xl text-2xl text-primary'>{heading}</h2>
+        <div className='h-[2px] w-[30%] bg-white m-auto'></div>
+        <div className='lg:gap-y-2 lg:grid lg:grid-cols-2 flex flex-col gap-2 py-8 w-[50%] m-auto'>
+          {brands.map((brand) => (
+            <a className='text-base' href={`/${brand}`}>
+              {brand}
+            </a>
+          ))}
+        </div>
+        <ReactMarkdown className='lg:text-2xl text-xl text-primary'>
+          {subheading}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default LuxuryBrandsCard;
