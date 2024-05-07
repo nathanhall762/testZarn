6246cd060c5aa97cdcaf6295d724bea3b1ce02b7
EthanWakeford <ethanwakeford@gmail.com>
2024-05-07 14:29:38 -0500

    upadated header to be a bit more responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,17 +30,18 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
-    setIsScrolled(isNavOpen ? false : true);
   };
 
   return (
-    <header className='fixed top-0 z-50 w-screen transition-all duration-300 ease-in-out bg-transparent backdrop-blur-sm backdrop-brightness-200 backdrop-contrast-[.15] backdrop-saturate-0'>
+    <header className='fixed top-0 z-50 w-screen transition-all duration-300 ease-in-out bg-transparent'>
       <div
-        className={`z-0 w-screen flex justify-around transition-all duration-1000 ${isScrolled ? 'h-0 opacity-0' : 'h-8'}`}>
+        className={`z-0 bg-dkbg1 w-screen flex justify-around items-center transition-all duration-300 border-b-[1px] border-mdbg1 transition-height ${isScrolled ? 'h-0 border-b-0' : 'h-8'}`}
+      >
         <a
-          className='flex-grow flex hover:scale-100'
-          href='tel:TELEPHONE_NUMBER_GOES_HERE'>
-          <div className='group cursor-pointer flex-grow flex justify-center gap-2 lg:gap-4 bg-dkbg1 mr-[0.4px] py-2'>
+          className='flex-grow flex items-center hover:scale-100 border-r-[1px] border-mdbg1 h-full'
+          href='tel:TELEPHONE_NUMBER_GOES_HERE'
+        >
+          <div className='group cursor-pointer flex-grow flex justify-center gap-2 lg:gap-4 bg-dkbg1'>
             <Icon
               icon='akar-icons:phone'
               className='text-accent group-hover:text-accent group-hover:scale-125 transition-all duration-300'
@@ -51,9 +52,10 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
           </div>
         </a>
         <a
-          className='flex-grow flex hover:scale-100'
-          href='mailto:EMAIL_ADDRESS_GOES_HERE'>
-          <div className='group cursor-pointer flex-grow flex justify-center lg:gap-4 bg-dkbg1 mx-[0.4px] py-2'>
+          className='flex-grow flex items-center hover:scale-100 border-r-[1px] border-mdbg1 h-full'
+          href='mailto:EMAIL_ADDRESS_GOES_HERE'
+        >
+          <div className='group cursor-pointer flex-grow flex justify-center lg:gap-4 bg-dkbg1'>
             <Icon
               icon='mdi:email'
               className='text-accent group-hover:text-accent group-hover:scale-125 transition-all duration-300'
@@ -64,9 +66,10 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
           </div>
         </a>
         <a
-          className='flex-grow flex hover:scale-100'
-          href='/'>
-          <div className='group cursor-pointer flex-grow flex justify-center lg:gap-4 bg-dkbg1 ml-[0.4px] py-2'>
+          className='flex-grow flex items-center hover:scale-100 h-full'
+          href='/'
+        >
+          <div className='group cursor-pointer flex-grow flex justify-center lg:gap-4 bg-dkbg1'>
             <Icon
               icon='mdi:map-marker'
               className='text-accent group-hover:text-accent group-hover:scale-125 transition-all duration-300'
@@ -78,49 +81,52 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
         </a>
       </div>
       <div
-        className={`z-50 relative ${isScrolled ? 'top-0' : 'top-0'} z-40 flex w-screen justify-between text-center align-middle shadow-2xl transition-all duration-1000 mb-[0.4px] lg:grid lg:grid-cols-12`}>
-        <div className='items-center flex-grow justify-center align-middle lg:col-span-3 lg:flex bg-dkbg1 mr-[0.4px] mt-[0.4px]'>
+        className={`relative top-0 flex w-screen justify-between text-center align-middle shadow-2xl transition-all duration-1000 lg:grid lg:grid-cols-12`}
+      >
+        <div className='items-center flex-grow justify-center align-middle lg:col-span-3 lg:flex bg-dkbg1  border-r-[1px] border-mdbg1'>
           <h2 className='text-center text-2xl text-black transition-all duration-300 hover:text-accent dark:text-white'>
             <a href='/'>{title}</a>
           </h2>
         </div>
-        <nav className='hidden items-center justify-center gap-6 align-middle lg:col-span-6 lg:flex bg-dkbg1 mx-[0.4px] mt-[0.4px]'>
+        <nav className='hidden items-center justify-center gap-6 align-middle lg:col-span-6 lg:flex bg-dkbg1  border-r-[1px] border-mdbg1'>
           {navs.map((nav) => (
             <a
               className='text-bold text-base text-black transition duration-300 ease-in-out hover:text-accent dark:text-white'
               href={`${nav.link}`}
-              key={nav.name}>
+              key={nav.name}
+            >
               {nav.name}
             </a>
           ))}
         </nav>
         <div
           onClick={toggleNav}
-          className={`z-50 flex w-10 cursor-pointer items-center justify-center mx-[0.4px] mt-[0.4px] text-2xl text-black bg-dkbg1 transition-transform duration-300 lg:hidden dark:text-white ${isNavOpen ? 'rotate-180 opacity-100' : 'rotate-0 opacity-100'}`}>
+          className={`z-50 flex w-10 cursor-pointer items-center justify-center text-2xl text-black bg-dkbg1 transition-transform duration-300 lg:hidden dark:text-white ${isNavOpen ? 'rotate-180 opacity-100' : 'rotate-0 opacity-100'}`}
+        >
           {isNavOpen ? <Icon icon={closeIcon} /> : <Icon icon={menuIcon} />}
         </div>
         <div
-          className={`absolute left-0 top-full z-50 w-full transform bg-white shadow-md lg:hidden ${isNavOpen ? 'scale-y-100' : 'scale-y-0'} origin-top transition-transform duration-300 ease-in-out`}>
+          className={`absolute left-0 top-full z-50 w-full transform bg-white shadow-md lg:hidden ${isNavOpen ? 'scale-y-100' : 'scale-y-0'} origin-top transition-transform duration-300 ease-in-out`}
+        >
           <nav className='flex flex-col dark:bg-dkbg1'>
             {navs.map((nav, index) => (
               <a
                 key={index}
                 href={`${nav.link}`}
-                className='px-6 py-4 text-base text-black dark:bg-dkbg1 dark:text-white'>
+                className='px-6 py-4 text-base text-black dark:bg-dkbg1 dark:text-white'
+              >
                 {nav.name}
               </a>
             ))}
-            <a
-              href='/contact'
-              className=''>
+            <a href='/contact' className=''>
               <button className='my-2 mb-4 transform rounded-xl bg-primary px-4 py-2 text-base transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
                 CONTACT US
               </button>
             </a>
           </nav>
         </div>
-        <div className='hidden lg:col-span-3 lg:flex lg:justify-around bg-dkbg1 px-4 m-[0.4px]'>
-          <div className='group cursor-pointer flex justify-center self-center gap-2 lg:gap-4 bg-dkbg1 ml-[0.4px] mt-[0.4px] py-2'>
+        <div className='hidden lg:col-span-3 lg:flex lg:justify-around bg-dkbg1 px-4'>
+          <div className='group cursor-pointer flex justify-center self-center gap-2 lg:gap-4 bg-dkbg1 py-2'>
             <Icon
               icon='akar-icons:phone'
               className='text-accent text-2xl group-hover:text-accent group-hover:scale-125 transition-all duration-300'
@@ -129,9 +135,7 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
               (918) 123-1234
             </h3>
           </div>
-          <a
-            href='/contact'
-            className=''>
+          <a href='/contact' className=''>
             <button className='transform rounded-xl bg-primary px-4 py-2 my-2 text-lg transition duration-300 ease-in-out hover:scale-110 hover:bg-accent'>
               CONTACT US
             </button>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-	  fontFamily: {
-		sans: ['Calibre', 'sans-serif'],
-		serif: ['Merriweather', 'serif'],
-	  },
-	  extend: {
-		colors: {
-		  black: '#0f0f0f',
-		  white: '#f5f5f5',
-		  primary: '#CF2E2E',
-		  secondary: '#002642',
-		  tertiary: '#840032',
-		  accent: '#FE0000',
-		  dkbg1: '#0f0f0f',
-		  dkbg2: '#1f1f1f',
-		  ltbg1: '#f5f5f5',
-		  ltbg2: '#e5e5e5',
-		},
-		height: {
-		  '3/4vh': '75vh',
-		},
-		transitionDuration: {
-		  '2000': '2000ms',
-		},
-	  },
-	},
-	plugins: [],
-  };
-  
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Calibre', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        black: '#0f0f0f',
+        white: '#f5f5f5',
+        primary: '#CF2E2E',
+        secondary: '#002642',
+        tertiary: '#840032',
+        accent: '#FE0000',
+        dkbg1: '#0f0f0f',
+        dkbg2: '#1f1f1f',
+        ltbg1: '#f5f5f5',
+        ltbg2: '#e5e5e5',
+        mdbg1: '#828282',
+      },
+      height: {
+        '3/4vh': '75vh',
+      },
+      transitionDuration: {
+        2000: '2000ms',
+      },
+    },
+  },
+  plugins: [],
+};
