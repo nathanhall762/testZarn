e0a422705011f325212e326352cdc5443762707b
EthanWakeford <ethanwakeford@gmail.com>
2024-05-21 16:42:14 -0500

    fixed dates

--- src/components/ReviewCards.tsx
+++ src/components/ReviewCards.tsx
@@ -22,7 +22,6 @@ interface Review {
   publishTime: string;
 }
 
-
 interface Props {
   heading: string;
 }
@@ -30,7 +29,6 @@ interface Props {
 const ReviewCards: React.FC<Props> = ({ heading }) => {
   const [reviews, setReviews] = useState<Review[]>([]);
   const apiKey = import.meta.env.PUBLIC_GOOGLE_PLACES_API_KEY;
-  
 
   useEffect(() => {
     // Define the function to fetch reviews from the Google Places API
@@ -53,26 +51,41 @@ const ReviewCards: React.FC<Props> = ({ heading }) => {
     return () => {};
   }, []); // Empty dependency array to run the effect only once
 
+  const formatTimestamp = (timestamp: string): string | null => {
+    const date = new Date(timestamp);
+
+    // Check if the date is invalid
+    if (isNaN(date.getTime())) {
+      return null;
+    }
+
+    return date.toLocaleDateString('en-US');
+  };
+
   return (
     <div className='pt-8'>
       <h2 className='pb-8 text-center text-2xl'>{heading}</h2>
-      <div className='scroller-sm snap-mandatory snap-x overflow-x-scroll flex lg:gap-16 gap-8 mx-auto px-24 lg:px-16 py-8'>
+      <div className='scroller-sm mx-auto flex snap-x snap-mandatory gap-8 overflow-x-scroll px-24 py-8 lg:gap-16 lg:px-16'>
         {reviews.map((review, index) => (
           <div
             key={index}
-            className='bg-primary rounded-lg bg-neutral-3 dark:bg-neutral-7 max-w-[350px] max-h-[400px] w-[70vw] h-[50vh] lg:p-6 p-4 text-pretty snap-always snap-center flex-shrink-0 overflow-hidden shadow-xl'
+            className='bg-primary h-[50vh] max-h-[400px] w-[70vw] max-w-[350px] flex-shrink-0 snap-center snap-always overflow-hidden text-pretty rounded-lg bg-neutral-3 p-4 shadow-xl lg:p-6 dark:bg-neutral-7'
           >
             <div className='flex justify-between pb-6 text-sm'>
               <img
                 alt={review.authorAttribution.displayName}
                 // style={{ backgroundImage: `url(${review.authorAttribution.photoUri})` }}
                 src={review.authorAttribution.photoUri}
-                className='rounded-full lg:size-24 size-16 bg-center bg-contain bg-no-repeat'
+                className='size-16 rounded-full bg-contain bg-center bg-no-repeat lg:size-24'
               />
               <div className='flex flex-col justify-evenly'>
-                <p className='lg:text-lg text-base'>{review.authorAttribution.displayName}</p>
+                <p className='text-base lg:text-lg'>
+                  {review.authorAttribution.displayName}
+                </p>
                 {/* <p className='text-xs lg:text-sm'>{review.count} reviews</p> */}
-                <p className='lg:text-sm text-xs'>{review.publishTime}</p>
+                <p className='text-xs lg:text-sm'>
+                  {formatTimestamp(review.publishTime)}
+                </p>
                 <div className='flex'>
                   {Array.from({ length: 5 }).map((_, index) => (
                     <Icon
@@ -84,7 +97,9 @@ const ReviewCards: React.FC<Props> = ({ heading }) => {
               </div>
             </div>
             <div className='line-clamp-[8]'>
-              <p className='inline text-justify h-full text-base'>{review.text.text}</p>
+              <p className='inline h-full text-justify text-base'>
+                {review.text.text}
+              </p>
             </div>
           </div>
         ))}
