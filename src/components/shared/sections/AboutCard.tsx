0e1ebf5a554d8b5b58fd24f39dec3808a61bd93f
EthanWakeford <ethanwakeford@gmail.com>
2024-07-01 18:54:33 -0500

    updated about card comp

--- src/components/shared/sections/AboutCard.tsx
+++ src/components/shared/sections/AboutCard.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
 import ReactMarkdown from 'react-markdown';
 
 interface AboutCardProps {
@@ -19,14 +20,32 @@ const AboutCard: React.FC<AboutCardProps> = ({
 }) => {
   return (
     <div className='my-8 flex px-4 lg:my-16'>
-      <div className='mx-auto flex max-w-6xl justify-center bg-neutral-7 px-6 py-8 text-center shadow-md lg:px-16 lg:py-16'>
-        <div className='flex max-w-5xl flex-col items-center justify-around align-baseline'>
-          <ReactMarkdown components={{
-            p(props) {
-            const { node, ...rest } = props;
-            return <h2 className='mb-4' {...rest} />;
-          }}} className='mb-4 text-lg font-bold lg:text-3xl'>{aboutTitle}</ReactMarkdown>
-          <div className='mb-8 h-[2px] w-full max-w-2xl bg-primary-md1'></div>
+      <div
+        className='mx-auto flex max-w-7xl justify-center bg-neutral-7 bg-cover bg-center px-6 py-8 text-center shadow-md lg:px-16 lg:py-16'
+        style={{ backgroundImage: 'url(/aboutNeilBGImage.png)' }}
+      >
+        <div className='flex flex-col items-center justify-around align-baseline'>
+          <ReactMarkdown
+            components={{
+              p(props) {
+                const { node, ...rest } = props;
+                return <h2 className='mb-4' {...rest} />;
+              },
+            }}
+            className='mb-4 text-lg font-bold lg:text-3xl'
+          >
+            {aboutTitle}
+          </ReactMarkdown>
+          {/* <div className='mb-8 h-[2px] w-full max-w-2xl bg-primary-md1'></div> */}
+          <div>
+            {Array.from({ length: 8 }).map((_, index) => (
+              <Icon
+                icon='ph:parallelogram-fill'
+                key={index}
+                className='pl-1 text-lg text-primary-md2'
+              />
+            ))}
+          </div>
           <ReactMarkdown
             className='markdown text-start text-lg lg:text-2xl'
             components={{
