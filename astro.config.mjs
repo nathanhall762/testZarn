1b2fbcce8a05cd5c5a5bf26e6114dcc022d7217a
EthanWakeford <ethanwakeford@gmail.com>
2024-09-05 14:57:11 -0500

    added google analytics + partytown to manage it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   prefetch: true,
 });
